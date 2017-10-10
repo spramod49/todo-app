e02ea26edda3a055ee2c937e48d2641ab710aa07
Pramod Rao <spramod49@gmail.com>
2017-10-10 18:30:08 +0530

    Finished project

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+// Check off specific todos by clicking.
+$('ul').on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+$('ul').on("click","span",function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todo = $(this).val();
+		$(this).val("");
+		$('ul').append("<li><span><i class='fa fa-trash'></i></span> " + todo + "</li>");
+	}
+});
+
+$('.fa-plus').click(function(){
+	$("input[type='text']").fadeToggle();
+});
